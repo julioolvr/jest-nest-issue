a59116a0a6766ed97417124daaa99e467f414046
Julio <julio.olvr@gmail.com>
2022-03-13 15:20:07 +0100

    Handle leak minimal example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bull';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    BullModule.registerQueue({
+      name: 'test-queue',
+    }),
+  ],
+})
+export class QueueModule {}
+
+@Module({
+  imports: [
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+    QueueModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
